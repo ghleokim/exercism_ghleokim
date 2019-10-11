dee0f318cdad8417716d8cceb4eff5b55e76ddbb
Leo Kim <gh.leokim@gmail.com>
2019-10-11 16:35:34 +0900

    solve isogram in python

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,11 @@
 def is_isogram(string):
-    pass
+    if not string: return True
+    
+    arr = [0 for _ in range(256)]
+    for char in string:
+        cur = ord(char.lower())
+        if cur == 32 or cur == 45: continue
+        elif arr[cur]: return False
+        else: arr[cur] += 1
+    
+    return True
