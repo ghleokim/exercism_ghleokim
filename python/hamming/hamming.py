7370b3cd845e39016a045d45727a5b36b2965b2b
Leo Kim <gh.leokim@gmail.com>
2019-10-11 17:22:11 +0900

    solve hamming in python

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,11 @@
 def distance(strand_a, strand_b):
-    pass
+    count = 0
+    al, bl = len(strand_a), len(strand_b)
+    if al != bl:
+        raise ValueError(r".+")
+
+    for i in range(al):
+        if strand_a[i] != strand_b[i]:
+            count += 1
+    
+    return count
